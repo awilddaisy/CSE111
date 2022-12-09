30f5539b3dd1430d15857c5974e7fb71761098c0
awilddaisy <mue19002@byui.edu>
2022-12-09 16:53:27 -0700

    changes

---
+++ grading.py
@@ -0,0 +1,107 @@
+# Climbing Route Consensus Averaging
+ ## This program will create exact and accurate averaging on routes based on climber's consensus.
+
+import math
+
+def main():
+    input_grades = []
+    # For easy testing 
+    # input_grades = ["5.7", "5.7+", "5.8", "5.9", "5.9+", "5.10-","5.6","5.7","5.8","5.9-"]
+
+    # User inputs grades
+    while len(input_grades) <= 9 :
+        input_grade=input("Enter your grade: ")
+        input_grades.append(input_grade)
+    grades_dict = make_grade_dict()
+    keys_dict = make_key_dict()
+
+    GRADE_INDEX = 0
+    VALUE_INDEX = 1
+
+    num_list = []
+    # Take the input values and create a list
+    for count in input_grades:
+        num=grades_dict[count]
+        num_list.append(num[0])
+    # print(num_list)
+
+    # Calculate the sum of the list above and divde by 10
+    # to find the average
+    grade_sum = sum(num_list)
+    grade_sum = int(grade_sum)
+    average = (grade_sum) / 10
+    average = round(average)
+
+    # Pulls calculated average from diff. dictionary
+    average = str(average)
+    # print(average)
+    get_average = keys_dict[average]
+    print(f"The exact grade is: {get_average}.")
+    
+def make_grade_dict():
+    grade_dict = {
+        "5.6-": [0],
+        "5.6": [1],
+        "5.6+" : [2],
+        "5.7-" : [3],
+        "5.7" : [4],
+        "5.7+" : [5],
+        "5.8-" : [6],
+        "5.8" : [7],
+        "5.8+" : [8],
+        "5.9-" : [9],
+        "5.9" : [10],
+        "5.9+" : [11],
+        "5.10-" : [12],
+        "5.10" : [13],
+        "5.10+" : [14],
+        "5.11-" : [15],
+        "5.11" : [16],
+        "5.11+" : [17],
+        "5.12-" : [18],
+        "5.12" : [19],
+        "5.12+" : [20],
+        "5.13-" : [21],
+        "5.13" : [22],
+        "5.13+" : [23],
+        "5.14-" : [24],
+        "5.14" : [25]
+    }
+    return grade_dict
+    # print(grade_dict)
+    print(grade_dict.values())
+
+def make_key_dict():
+    key_dict = {
+        "0" : ['5.6-'],
+        "1" : ['5.6'],
+        "2" : ['5.6'],
+        "3" : ['5.7-'],
+        "4" : ['5.7'],
+        "5" : ['5.7+'],
+        "6" : ['5.8-'],
+        "7" : ['5.8'],
+        "8" : ['5.8+'],
+        "9" : ['5.9-'],
+        "10" : ['5.9'],
+        "11" : ['5.9+'],
+        "12" : ['5.10-'],
+        "13" : ['5.10'],
+        "14" : ['5.10+'],
+        "15" : ['5.11-'],
+        "16" : ['5.11'],
+        "17" : ['5.11+'],
+        "18" : ['5.12-'],
+        "19" : ['5.12'],
+        "20" : ['5.12+'],
+        "21" : ['5.13-'],
+        "22" : ['5.13'],
+        "23" : ['5.13+'],
+        "24" : ['5.14-'],
+        "25": ['5.14']
+        }
+    return key_dict
+
+main()
+
+
--- receipts.py
+++ receipts.py
@@ -1,23 +1,47 @@
 import csv
+from datetime import datetime
 
 def main():
-    products_index=3
-    products_dict=read_dict("products.csv", products_index)
+    current_date_and_time = datetime.now()
+    PRODUCTS_INDEX=0
+    products_dict=read_dict("products.csv", PRODUCTS_INDEX)
     
-    print(products_dict)
-    
+    # print(products_dict)
+    PRODUCT_NUM_INDEX=0
+    QUANTITY_INDEX=1
+    total_quantity=0
+    subtotal=0
+    total_amount=0
+    sales_tax=float(0.92)
+    print()
+    print("Hello! Welcome to your online shopping experience!")
+    print()
+    print("YOUR RECEIPT:")
+    with open("request.csv", "rt") as csv_file:
+        reader=csv.reader(csv_file)
+        next(reader)
+        for row_list in reader:
+            product_number = row_list[PRODUCT_NUM_INDEX]
+            product_quantity = int(row_list[QUANTITY_INDEX])
+            quantity=int(row_list[QUANTITY_INDEX])
+            product_name = products_dict[product_number]
+            print(f"{product_name[1]}: {product_quantity} @ {product_name[2]}")
+            total_quantity += quantity
+            subtotal +=float(quantity) * float(product_name[2])
+            total_amount = subtotal + sales_tax
+            
+        print()
+        print(f"Number of Items: {total_quantity}")
+        # print(f"Sales Tax: {sales_tax}")
+        print(f"Subtotal: ${subtotal:.2f}")
+        print(f"Sales Tax: ${sales_tax}")
+        print(f"Total: ${total_amount}")
+        print()
+        print("Thank you for shopping with us today, come again soon!")
+        print(f"{current_date_and_time:%A %I:%M %p}")
 
 def read_dict(filename, key_column_index):
-    """Read the contents of a CSV file into a compound
-    dictionary and return the dictionary.
 
-    Parameters
-        filename: the name of the CSV file to read.
-        key_column_index: the index of the column
-            to use as the keys in the dictionary.
-    Return: a compound dictionary that contains
-        the contents of the CSV file.
-    """
     dictionary= {}
     with open(filename, "rt") as csv_file:
         reader=csv.reader(csv_file)
@@ -26,9 +50,7 @@ def read_dict(filename, key_column_index):
         for row_list in reader:
 
             if len(row_list) != 0:
-
                 key = row_list[key_column_index]
-
                 dictionary[key] = row_list
 
     return dictionary
